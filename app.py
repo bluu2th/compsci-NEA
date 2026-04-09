96de796161e9b366e2e9fbeae280572c3434db50
bluu2th <blumackenziestevens@gmail.com>
2026-04-09 23:53:38 +0100

    cleaning it up, ready to start on the actual project without as many testing features in the way

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, url_for
 import sqlite3
 import json
 
 app = Flask(__name__)
 
-# Testing routing
+# Home page
 @app.route("/")
 def hello_world():
-    return render_template("test.html")
+    return render_template("hello.html")
 
 
 # A page listing all mental health resources from the website
 @app.route("/resources/")
 def resources():
     the_resources = [
-    {"img_url": "cattest.webp", "width": 640, "height": 480},
-    {"img_url": "testcat.jpg", "width": 1920, "height": 1080},
+    
 ]
     return render_template("resources.html", resources = the_resources)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+# Test route 
+@app.route("/hello/")
+@app.route("/hello/<name>")
 def hello(name=None):
-    return render_template('hello.html', person=name)
+    return render_template("hello.html", person=name)
 
+# 
+@app.route("")
